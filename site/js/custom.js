789338510e74a706f1764324566e7d3e0bcf5e71
Jesus Castillo <jesuscmd@gmail.com>
2015-05-20 04:53:46 -0500

    cambios finales

--- site/js/custom.js
+++ site/js/custom.js
@@ -302,6 +302,49 @@ $(document).ready(function() {
     });
 });
 
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// slider
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+
+$('.infography-slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 4,
+        initialSlide:1,
+        centerMode: true
+      }
+    }
+    , {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true
+      }
+    }
+
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
 /* ////////////////////////////////////////////////////////////////////////////
 //
 // resize
